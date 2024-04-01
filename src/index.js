43e0abb4db7d596a0ddca8079ae00d90a31de074
Demend2607 <finder2607011@gmail.com>
2024-04-01 11:23:20 +1000

    route

--- src/index.js
+++ src/index.js
@@ -6,16 +6,16 @@ import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { store, persistor } from './store/store';
 
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<Provider store={store}>
 		<PersistGate loading={null} persistor={persistor}>
-			<BrowserRouter>
+			<HashRouter>
 				<App />
-			</BrowserRouter>
+			</HashRouter>
 		</PersistGate>
 	</Provider>
 );
